69def78b4bb8bda82c61bc0647840fcb7164be71
Rajevic Mile <rajevic.mile6@gmail.com>
2026-04-08 00:18:09 +0200

    feat(stat-system): add StatReader read-only view (ISP)

---
+++ addons/stat_system/core/stat_reader.gd
@@ -0,0 +1,41 @@
+extends RefCounted
+class_name StatReader
+## Read-only view of a StatBlock for UI consumers.
+## Exposes only reads + signal forwarding. UI bars and buff lists should
+## depend on this, not on StatComponent or StatBlock directly.
+##
+## Construct via StatComponent.get_reader().
+
+signal stat_changed(id: StringName, old_value: float, new_value: float)
+signal resource_depleted(id: StringName)
+signal resource_filled(id: StringName)
+
+var _block: StatBlock
+
+func _init(block: StatBlock) -> void:
+	_block = block
+	if _block:
+		_block.stat_changed.connect(_forward_stat_changed)
+		_block.resource_depleted.connect(_forward_depleted)
+		_block.resource_filled.connect(_forward_filled)
+
+func get_value(id: StringName) -> float:
+	return _block.get_value(id) if _block else 0.0
+
+func get_max(id: StringName) -> float:
+	return _block.get_max(id) if _block else 0.0
+
+func get_current(id: StringName) -> float:
+	return _block.get_current(id) if _block else 0.0
+
+func get_active_modifiers() -> Array[StatModifier]:
+	return _block.get_active_modifiers() if _block else []
+
+func _forward_stat_changed(id: StringName, old_value: float, new_value: float) -> void:
+	stat_changed.emit(id, old_value, new_value)
+
+func _forward_depleted(id: StringName) -> void:
+	resource_depleted.emit(id)
+
+func _forward_filled(id: StringName) -> void:
+	resource_filled.emit(id)
